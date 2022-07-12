a53328a4c175dc8255497c92102c820de432537f
Dario Del Piano <dario@metacell.us>
2022-07-12 15:43:39 +0100

    #5 migrate dockbar to library, plus some versioning fixes, plus support for svgs in tsdx

---
+++ src/components/assets/styles/variables.ts
@@ -0,0 +1,37 @@
+const vars = {
+  fontFamily: 'Inter, sans-serif',
+  primaryBg: '#f1f1f1',
+  textWhite: '#FFFFFF',
+  chipTextColor: '#F2F2F7',
+  chipBgColor: 'rgba(60, 60, 67, 0.4)',
+
+  chipPrimaryTextColor: 'rgba(255, 255, 255, 0.8)',
+  chipPrimaryBgColor: 'rgba(0, 122, 255, 0.6)',
+
+  breadcrumbLinkColor: '#A2A2A2',
+  breadcrumbTextColor: '#292929',
+
+  buttonPrimaryBgColor: '4353FF',
+  buttonPrimaryBgHoverColor: '#3443E1',
+  buttonPrimaryDisabledBgColor: 'rgba(0, 122, 255, 0.4)',
+
+  listItemActiveBg: '#007AFF',
+  listSelectedTextColor: '#3C3C43',
+  listBoxShadow: '0 0.1875rem 0.5rem rgba(0, 0, 0, 0.12), 0 0.1875rem 0.0625rem rgba(0, 0, 0, 0.04)',
+  listBorderColor: 'rgba(0, 0, 0, 0.04)',
+
+  dividerColor: 'rgba(118, 120, 125, 0.12)',
+
+  dropdownBg: 'rgba(246, 246, 248, 0.8)',
+  dropdownTextColor: 'rgba(60, 60, 67, 0.6)',
+
+  overlayColor: 'rgba(0, 0, 0, 0.4)',
+
+  progressBg: '#E5E5E5',
+  progressBar: '#017AFF',
+  progressShadow: 'inset 0 0 0.0625rem #E3E3E3',
+
+  switchShadow: '0 0.1875rem 0.5rem rgba(0, 0, 0, 0.15), 0 0.1875rem 0.0625rem rgba(0, 0, 0, 0.06)'
+};
+
+export default vars;
