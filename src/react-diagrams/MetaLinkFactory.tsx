1f04c2f54fec8c91d7c10ed82b1cb442ff5520f7
afonso <afonsobspinto@gmail.com>
2022-07-20 12:10:35 +0100

    #10 chore: Fix lint

--- src/react-diagrams/MetaLinkFactory.tsx
+++ src/react-diagrams/MetaLinkFactory.tsx
@@ -30,7 +30,12 @@ export class MetaLinkFactory extends DefaultLinkFactory {
 
       return (
         // @ts-ignore
-        <ReactComponentType engine={this.engine} model={model} path={path} selected={selected} />
+        <ReactComponentType
+          engine={this.engine}
+          model={model}
+          path={path}
+          selected={selected}
+        />
       );
     }
     // TODO: Generate default link instead
