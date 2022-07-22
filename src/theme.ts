319783180e917cc925ef30c30f4d07a16a1b08c2
vidhya-metacell <vidhya@metacell.us>
2022-07-22 18:04:47 +0200

    #16  Add more variables for sidebar customisation, move styles for nodes to meta-diagram

--- src/theme.ts
+++ src/theme.ts
@@ -15,6 +15,8 @@ const applicationTheme = (params: ThemeVars) => {
     listSelectedTextColor,
     listBoxShadow,
     listBorderColor,
+    sidebarBg,
+    sidebarShadow
   } = params;
   return {
     components: {
@@ -28,6 +30,119 @@ const applicationTheme = (params: ThemeVars) => {
             background-color:${primaryBg};
             font-family: ${fontFamily};
             font-size: 1rem;
+          }
+          .sidebar {
+            z-index: 5;
+            width: 4rem;
+            background: ${sidebarBg};
+            box-shadow: ${sidebarShadow};
+            border-radius: 2rem;
+            position: fixed;
+            left: 1rem;
+            top: 50%;
+            transform: translateY(-50%);
+          }
+
+          .sidebar.right {
+            left: auto;
+            right: 1rem;
+          }
+
+          .sidebar .MuiList-root {
+            padding: 0.75rem
+          }
+
+          .canvas-widget {
+            height: 100%;
+            width: 100%;
+            background-color: #fff;
+          }
+
+          .node {
+            border: solid 0.0625rem;
+            border-radius: 50%;
+            width: 10rem;
+            height: 10rem;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            position: relative;
+          }
+
+          .node img {
+            margin-bottom: 0.25rem
+          }
+
+          .node p {
+            font-weight: 500;
+            font-size: 0.8125rem;
+            line-height: 1.25rem;
+            letter-spacing: -0.005rem;
+            margin: 0;
+          }
+
+          .node-button.MuiButton-root {
+            background-color: #161A1E;
+            border-radius: 1.125rem;
+            font-weight: 600;
+            font-size: 0.8125rem;
+            display: flex;
+            line-height: 1rem;
+            letter-spacing: -0.025625rem;
+            color: rgba(255, 255, 255, 0.8);
+            margin: 0 !important;
+            width: 9.625rem;
+            padding: 0;
+            height: 2.25rem;
+            position: absolute;
+            top: -2.625rem;
+            font-family: ${fontFamily};
+            text-transform: none;
+            left: 50%;
+            transform: translateX(-50%);
+          }
+
+          .node-button.MuiButton-root:hover {
+            background-color: #161A1E;
+          }
+
+          .node-button .icon {
+            width: 1rem;
+            border: solid 0.0625rem;
+            height: 1rem;
+            border-radius: 50%;
+            margin-right: 1.25rem;
+            position: relative;
+          }
+
+          .node-button .icon:after {
+            content: "";
+            height: 1.75rem;
+            width: 0.0625rem;
+            display: block;
+            position: absolute;
+            right: -0.625rem;
+            top: 50%;
+            transform: translateY(-50%);
+            background-color: rgba(255, 255, 255, 0.2);
+          }
+
+          .nodes {
+            width: 10rem;
+            height: 10rem;
+            border: 0.09375rem solid #18A0FB;
+            z-index: 99999;
+            position: absolute;
+          }
+
+          .node .pointer {
+            width: 0.625rem;
+            height: 0.625rem;
+            background: #FFFFFF;
+            border: 0.09375rem solid #18A0FB;
+            border-radius: 0.125rem;
+            position: absolute;
           }
         `,
       },
