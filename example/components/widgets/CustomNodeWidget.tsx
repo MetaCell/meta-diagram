113653db440c08f749b3bf2311986d0919e2503c
vidhya-metacell <vidhya@metacell.us>
2022-07-22 18:06:16 +0200

    #16 Add Nodes UI and color variants

--- example/components/widgets/CustomNodeWidget.tsx
+++ example/components/widgets/CustomNodeWidget.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import {DiagramEngine, PortWidget} from "@projectstorm/react-diagrams";
 import {MetaNodeModel} from "../../../.";
-import {Typography} from "@material-ui/core";
+import {Box, Button, Typography} from "@material-ui/core";
 
 export interface CustomNodeWidgetProps {
     model: MetaNodeModel;
@@ -9,53 +9,64 @@ export interface CustomNodeWidgetProps {
 }
 
 export class CustomNodeWidget extends React.Component<CustomNodeWidgetProps> {
-
     render() {
-        // @ts-ignore
-        const customNodeStyle = {
-            zIndex: 999999999,
-            border: "solid 2px gray",
-            borderRadius: "5px",
-            width: "250px",
-            height: "200px",
-            display: "flex",
-            alignItems: "flex-start",
-            justifyContent: "space-between",
-            position: "relative",
-            background: this.props.model.getOptions()['color'] || "darkgray",
-            top: this.props.model.getOptions()['position']?.y || 0,
-            left: this.props.model.getOptions()['position']?.x || 0
-        }
-
-        const circlePortStyle = {
-            width: "12px",
-            height: "12px",
-            margin: "2px",
-            borderRadius: "4px",
-            background: "darkgray",
-            cursor: "pointer",
-        }
-
+        const customNodeStyles = {
+          background: this.props.model.getOptions()['backgroundColor'],
+          borderColor: this.props.model.getOptions()['borderColor'],
+          boxShadow: this.props.model.getOptions()['backgrounboxShadowdColor'],
+        };
         return (
-            <>
-                <div style={customNodeStyle}>
-                    <PortWidget
-                        style={{position: 'absolute', top: '0px', left: '0px'}}
-                        engine={this.props.engine}
-                        port={this.props.model.getPort("in")}
-                    >
-                        <div style={circlePortStyle}/>
-                    </PortWidget>
-                    <PortWidget
-                        style={{position: 'absolute', top: '0px', right: '0px'}}
-                        engine={this.props.engine}
-                        port={this.props.model.getPort("out")}
-                    >
-                        <div style={circlePortStyle}/>
-                    </PortWidget>
-                    <Typography>{this.props.model.getOptions()['name']}</Typography>
-                </div>
-            </>
+          <Box position='relative'>
+            {this.props.model.getOptions()['selected'] && (
+              <>
+                <Button className="node-button">
+                  <Box
+                    className="icon"
+                    style={customNodeStyles}
+                  />
+                  Show properties
+                </Button>
+
+                <Box className="nodes">
+                  <PortWidget
+                    engine={this.props.engine}
+                    port={this.props.model.getPort("in")}
+                  >
+                    <Box style={{ left: '-0.375rem', top: '-0.375rem' }} className="pointer" />
+                  </PortWidget>
+                  <PortWidget
+                    engine={this.props.engine}
+                    port={this.props.model.getPort("out")}
+                  >
+                    <Box style={{ right: '-0.375rem', top: '-0.375rem' }} className="pointer" />
+                  </PortWidget>
+
+                  <PortWidget
+                    engine={this.props.engine}
+                    port={this.props.model.getPort("in")}
+                  >
+                    <Box style={{ left: '-0.375rem', bottom: '-0.375rem' }} className="pointer" />
+                  </PortWidget>
+                  <PortWidget
+                    engine={this.props.engine}
+                    port={this.props.model.getPort("out")}
+                  >
+                    <Box style={{ bottom: '-0.375rem', right: '-0.375rem' }} className="pointer" />
+                  </PortWidget>
+                </Box>
+              </>
+            )}
+
+            <Box
+              className="node"
+              style={customNodeStyles}
+            >
+              <img src={this.props.model.getOptions()['icon']} alt={this.props.model.getOptions()['name']} />
+              <Typography component="p" style={{ color: this.props.model.getOptions()['textColor'] }}>
+                {this.props.model.getOptions()['name']}
+              </Typography>
+            </Box>
+          </Box>
         );
     }
 }
--- example/index.tsx
+++ example/index.tsx
@@ -6,26 +6,27 @@ import {CustomNodeWidget} from "./components/widgets/CustomNodeWidget";
 import {makeStyles} from "@material-ui/core";
 import CustomLinkWidget from "./components/widgets/CustomLinkWidget";
 import BG from "./components/assets/svg/bg-dotted.svg";
+import { colorGreen, colorOrange } from "./components/assets/styles/constants";
 
 const useStyles = makeStyles(_ => ({
-    main: {
-        position: 'absolute',
-        height: '100%',
-        width: '100%',
-    },
-    canvasBG: {
-        backgroundImage: `url(${BG})`
-    }
-}))
+  main: {
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+  },
+  canvasBG: {
+    backgroundImage: `url(${BG})`
+  }
+}));
 
 const App = () => {
     const classes = useStyles();
 
-    const node1 = new MetaNode('1', 'node1', 'default', new Position(250, 100),
-        new Map(Object.entries({color: 'rgb(0,192,255)'})))
+    const node1 = new MetaNode('1', 'Node 1', 'default', new Position(250, 100),
+        new Map(Object.entries(colorGreen)))
 
-    const node2 = new MetaNode('2', 'node2', 'default', new Position(500, 100),
-        new Map(Object.entries({color: 'rgb(255,192,0)'})))
+    const node2 = new MetaNode('2', 'Node 2', 'default', new Position(500, 100),
+        new Map(Object.entries(colorOrange)))
 
     const link3 = new MetaLink('3', 'link3', 'default', '1', 'out', '2', 'in',
         new Map(Object.entries({color: 'rgb(255,192,0)'})))
@@ -37,12 +38,12 @@ const App = () => {
 
     return (
         <div className={classes.main}>
-            <MetaDiagram metaNodes={[node1, node2]} metaLinks={[link3]} componentsMap={componentsMap}
-                metaTheme={{
-                    customThemeVariables: {},
-                    canvasClassName: classes.canvasBG,
-                }}
-            />
+          <MetaDiagram metaNodes={[node1, node2]} metaLinks={[link3]} componentsMap={componentsMap}
+            metaTheme={{
+                customThemeVariables: {},
+                canvasClassName: classes.canvasBG,
+            }}
+          />
         </div>
     );
 };
