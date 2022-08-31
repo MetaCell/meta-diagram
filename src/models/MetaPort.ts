40b3595cd7759c5e9c2948119311747daa8056aa
Dario Del Piano <dario@metacell.us>
2022-08-31 14:43:16 +0100

    #30 fixing linter issues 2

--- src/models/MetaPort.ts
+++ src/models/MetaPort.ts
@@ -2,36 +2,36 @@ import { Position } from './Position';
 import { PortTypes } from '../constants';
 
 export class MetaPort {
-    id: string;
-    name: string;
-    type: PortTypes;
-    position: Position;
+  id: string;
+  name: string;
+  type: PortTypes;
+  position: Position;
 
-    constructor(
-        id: string,
-        name: string,
-        type: PortTypes,
-        position: Position,
-        options: Map<string, any>
-    ) {
-        if (options === undefined) {
-            options = new Map<string, any>();
-        }
-        this.id = id;
-        this.name = name;
-        this.type = type;
-        this.position = position;
+  constructor(
+    id: string,
+    name: string,
+    type: PortTypes,
+    position: Position,
+    options: Map<string, any>
+  ) {
+    if (options === undefined) {
+      options = new Map<string, any>();
     }
+    this.id = id;
+    this.name = name;
+    this.type = type;
+    this.position = position;
+  }
 
-    getId(): string {
-        return this.id;
-    }
+  getId(): string {
+    return this.id;
+  }
 
-    getName(): string {
-        return this.name;
-    }
+  getName(): string {
+    return this.name;
+  }
 
-    getType(): string {
-        return this.type;
-    }
+  getType(): string {
+    return this.type;
+  }
 }
