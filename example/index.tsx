8189e2a7ca18ec7d2bbf0f869132013fa2bd0ea3
afonso <afonsobspinto@gmail.com>
2023-04-06 14:47:44 +0100

    PSYNEU-31 feat: Add sidebar to example app

--- example/index.tsx
+++ example/index.tsx
@@ -1,16 +1,25 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import MetaDiagram, { MetaNode, Position, ComponentsMap, MetaLink, MetaPort, PortTypes } from '@metacell/meta-diagram';
-// import {makeStyles} from "@material-ui/core";
+import MetaDiagram, {
+  MetaNode,
+  ComponentsMap,
+  MetaLink,
+  MetaPort,
+  PortTypes,
+  MetaNodeModel
+} from '@metacell/meta-diagram';
 import CustomLinkWidget from './components/widgets/CustomLinkWidget';
+// @ts-ignore
 import BG from './components/assets/svg/bg-dotted.svg';
-import CustomNodeWidget from './components/widgets/CustomNodeWidget';
 import { ExpandedCustomNodeWidget } from './components/widgets/ExpandedCustomNodeWidget';
 import { makeStyles } from '@mui/styles';
 import { ThemeProvider } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
+import {Point} from "@projectstorm/geometry";
+import FolderIcon from '@mui/icons-material/Folder';
+import { blue, red } from '@mui/material/colors';
 
 const useStyles = makeStyles(_ => ({
   main: {
@@ -23,6 +32,48 @@ const useStyles = makeStyles(_ => ({
   },
 }));
 
+let counter = 0
+
+export function onNodeDrop(monitor, node, engine) {
+  const options = new Map();
+  const position = monitor?.getClientOffset();
+  const newMetaNode = new MetaNode(
+      `${node.id}-${counter}`,
+      `${node.name}-${counter}`,
+      'default',
+      new Point(position.x, position.y),
+      node.type,
+      null,
+      [new MetaPort('in', 'in', PortTypes.INPUT_PORT, undefined, undefined), new MetaPort('out', 'out', PortTypes.OUTPUT_PORT, undefined, undefined)],
+      [],
+      options
+    );
+
+  if (engine) {
+      counter++
+      return engine.getModel().addNode(newMetaNode.toModel());
+  }
+}
+
+const leftSideBarNodes = [
+    {
+        id: 'blueNode',
+        name: 'Blue Node',
+        icon: <FolderIcon sx={{ color: blue[500] }}  />,
+        type: 'node-blue',
+        draggable: true,
+        onNodeDrop
+    },
+    {
+        id: 'redNode',
+        name: 'red Node',
+        icon: <FolderIcon sx={{ color: red[500] }} />,
+        type: 'node-red',
+        draggable: true,
+        onNodeDrop
+    },
+];
+
 const App = () => {
   const classes = useStyles();
 
@@ -30,7 +81,7 @@ const App = () => {
     'group2',
     'grandparent',
     'default',
-    new Position(130, 105),
+    new Point(130, 105),
     'node-red',
     null,
     [new MetaPort('in', 'in', PortTypes.INPUT_PORT, undefined, undefined), new MetaPort('out', 'out', PortTypes.OUTPUT_PORT, undefined, undefined)],
@@ -42,7 +93,7 @@ const App = () => {
     'group',
     'parent',
     'default',
-    new Position(130, 105),
+    new Point(130, 105),
     'node-blue',
     grandparent,
     [new MetaPort('in', 'in', PortTypes.INPUT_PORT, undefined, undefined), new MetaPort('out', 'out', PortTypes.OUTPUT_PORT, undefined, undefined)],
@@ -54,7 +105,7 @@ const App = () => {
     '1',
     'node1',
     'default',
-    new Position(130, 105),
+    new Point(130, 105),
     'node-red',
     parent,
     [new MetaPort('in', 'in', PortTypes.INPUT_PORT, undefined, undefined), new MetaPort('out', 'out', PortTypes.OUTPUT_PORT, undefined, undefined)],
@@ -66,7 +117,7 @@ const App = () => {
     '2',
     'node2',
     'default',
-    new Position(130, 210),
+    new Point(130, 210),
     'node-blue',
     parent,
     [new MetaPort('in', 'in', PortTypes.INPUT_PORT, undefined, undefined), new MetaPort('out', 'out', PortTypes.OUTPUT_PORT, undefined, undefined)],
@@ -105,6 +156,9 @@ const App = () => {
           metaTheme={{
             customThemeVariables: {},
             canvasClassName: classes.canvasBG,
+          }}
+          sidebarProps={{
+              sidebarNodes: leftSideBarNodes,
           }}
         />
       </div>
