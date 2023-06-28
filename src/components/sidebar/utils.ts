075e81edc49d373467bccba5c1c43514883b1655
ddelpiano <dario@metacell.us>
2023-06-28 20:41:06 +0100

    fixed linter errors

--- src/components/sidebar/utils.ts
+++ src/components/sidebar/utils.ts
@@ -1,24 +1,23 @@
-import { IHandleClick } from "../../types/sidebar";
-import { DefaultSidebarNodeTypes } from "../../constants";
-
+import { IHandleClick } from '../../types/sidebar';
+import { DefaultSidebarNodeTypes } from '../../constants';
 
 export const handleItemClick = ({
-    event,
-    node,
-    handleSelection
+  event,
+  node,
+  handleSelection,
 }: IHandleClick) => {
-    const { id, draggable, preCallback, postCallback, defaultCallback } = node;
-    // if item is un-draggable click event fires only
-    if (!draggable) {
-        handleSelection(id as DefaultSidebarNodeTypes);
+  const { id, draggable, preCallback, postCallback, defaultCallback } = node;
+  // if item is un-draggable click event fires only
+  if (!draggable) {
+    handleSelection(id as DefaultSidebarNodeTypes);
 
-        // execute pre & post-callback when no overriding default-callback
-        if (!!preCallback && !defaultCallback) preCallback(event, node);
-        if (!!postCallback && !defaultCallback) postCallback(event, node);
+    // execute pre & post-callback when no overriding default-callback
+    if (!!preCallback && !defaultCallback) preCallback(event, node);
+    if (!!postCallback && !defaultCallback) postCallback(event, node);
 
-        // call default - callback if exists
-        if (!!defaultCallback) defaultCallback(event, node);
-    }
+    // call default - callback if exists
+    if (!!defaultCallback) defaultCallback(event, node);
+  }
 };
 
 export default handleItemClick;
--- src/types/sidebar.d.ts
+++ src/types/sidebar.d.ts
@@ -1,62 +1,62 @@
-import React from "react";
-import { DropTargetMonitor } from "react-dnd";
-import { CanvasEngine } from "@projectstorm/react-canvas-core";
-import { DefaultSidebarNodeTypes } from "../constants";
-import { State } from "../components/sidebar/states/State";
+import React from 'react';
+import { DropTargetMonitor } from 'react-dnd';
+import { CanvasEngine } from '@projectstorm/react-canvas-core';
+import { DefaultSidebarNodeTypes } from '../constants';
+import { State } from '../components/sidebar/states/State';
 
 export type StateMap = {
-    [key in DefaultSidebarNodeTypes]?: State;
+  [key in DefaultSidebarNodeTypes]?: State;
 };
 
 export interface INode {
-    id: string;
-    icon: React.ReactElement;
-    name: string;
-    type: string;
-    draggable: boolean;
-    preCallback?: (event: any, node: unknown) => void;
-    postCallback?: (event: any, node: unknown) => void;
-    defaultCallback?: (event: any, node: unknown) => void;
-    onNodeDrop?: (
-        monitor: DropTargetMonitor,
-        node: unknown,
-        engine?: CanvasEngine
-    ) => void;
-    css?: React.CSSProperties;
-    children?: INode[];
+  id: string;
+  icon: React.ReactElement;
+  name: string;
+  type: string;
+  draggable: boolean;
+  preCallback?: (event: any, node: unknown) => void;
+  postCallback?: (event: any, node: unknown) => void;
+  defaultCallback?: (event: any, node: unknown) => void;
+  onNodeDrop?: (
+    monitor: DropTargetMonitor,
+    node: unknown,
+    engine?: CanvasEngine
+  ) => void;
+  css?: React.CSSProperties;
+  children?: INode[];
 }
 
 export interface ISidebarNodeProps extends INode {
-    divider?: boolean;
+  divider?: boolean;
 }
 
 interface IHandleSelection {
-    handleSelection: (id: DefaultSidebarNodeTypes) => void;
+  handleSelection: (id: DefaultSidebarNodeTypes) => void;
 }
 
 export interface SidebarItemProps extends IHandleSelection {
-    node: INode | ISidebarNodeProps;
-    selected?: boolean;
+  node: INode | ISidebarNodeProps;
+  selected?: boolean;
 }
 
 export interface ISubSidebarItemProps extends IHandleSelection {
-    node: INode | ISidebarNodeProps;
-    selected?: boolean;
+  node: INode | ISidebarNodeProps;
+  selected?: boolean;
 }
 
 export interface ISidebarProps {
-    selectedBarNode?: string;
-    sidebarNodes?: ISidebarNodeProps[];
-    updateSelection: (id: string) => void;
-    engine: CanvasEngine;
+  selectedBarNode?: string;
+  sidebarNodes?: ISidebarNodeProps[];
+  updateSelection: (id: string) => void;
+  engine: CanvasEngine;
 }
 
 export interface IHandleClick extends IHandleSelection {
-    event: unknown;
-    node: INode | ISidebarNodeProps;
+  event: unknown;
+  node: INode | ISidebarNodeProps;
 }
 
 export interface ISubSidebar {
-    show?: boolean;
-    nodes?: INode[];
+  show?: boolean;
+  nodes?: INode[];
 }
