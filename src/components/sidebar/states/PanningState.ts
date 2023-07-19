01da2da83ac33e77ec33646f879538eedc0e9456
emekauja <ujadninth@gmail.com>
2023-07-19 14:53:41 +0100

    chore: update panning state methods

--- src/components/sidebar/states/PanningState.ts
+++ src/components/sidebar/states/PanningState.ts
@@ -2,29 +2,24 @@ import { State } from './State';
 import { updateCanvasMouseCursor } from '../../../utils';
 import { CursorTypes } from '../../../constants';
 import { DefaultState } from '../../../react-diagrams/state/DefaultState';
-import { DefaultDiagramState } from '@projectstorm/react-diagrams';
 
 export class PanningState extends State {
   onExit() {
-    if (
-      this.state instanceof DefaultState ||
-      this.state instanceof DefaultDiagramState
-    ) {
-      this.state.dragCanvas.config.allowDrag = false;
-    } else {
-      this.state.config.allowDrag = false;
+    if (this.state instanceof DefaultState) {
+      if (this.state.dragCanvas) {
+        this.state.dragCanvas.config.allowDrag = false;
+      }
+      this.state.unsetDragState();
     }
     updateCanvasMouseCursor(CursorTypes.DEFAULT);
   }
 
   onEnter() {
-    if (
-      this.state instanceof DefaultState ||
-      this.state instanceof DefaultDiagramState
-    ) {
-      this.state.dragCanvas.config.allowDrag = true;
-    } else {
-      this.state.config.allowDrag = true;
+    if (this.state instanceof DefaultState) {
+      this.state.setDragState();
+      if (this.state.dragCanvas) {
+        this.state.dragCanvas.config.allowDrag = true;
+      }
     }
     updateCanvasMouseCursor(CursorTypes.MOVE);
   }
